f554b6649e58af1ba5a0c35a5bb242c4d73195f3
Andrew Jensen <AJensen@student.neumont.edu>
2020-11-13 20:23:22 -0700

    Create Basic Main Menu

---
+++ Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField] Sprite volumeOnSprite = null;
+    [SerializeField] Sprite volumeOffSprite = null;
+
+
+
+    private Button volumeButton = null;
+    private bool volumeOn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeButton = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void VolumeButtonClicked()
+    {
+        volumeButton.image.sprite = volumeOn ? volumeOffSprite : volumeOnSprite;
+
+        volumeOn = !volumeOn;
+
+    }
+
+    public void LeaderBoardButtonClicked()
+    {
+
+    }
+
+    public void EasyButtonClicked()
+    {
+
+    }
+
+    public void MediumButtonClicked()
+    {
+
+    }
+
+    public void HardButtonClicked()
+    {
+
+    }
+
+
+}
