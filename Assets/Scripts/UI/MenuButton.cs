549941de556646acbf31fa570ac3d0c441786496
Andrew Jensen <AJensen@student.neumont.edu>
2020-11-16 20:37:18 -0700

    Add game scene UI

--- Assets/Scripts/UI/MenuButton.cs
+++ Assets/Scripts/UI/MenuButton.cs
@@ -2,24 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour
 {
-    [SerializeField] Sprite volumeOnSprite = null;
-    [SerializeField] Sprite volumeOffSprite = null;
+    Sprite volumeOnSprite = null;
+    Sprite volumeOffSprite = null;
+
+    Sprite pauseSprite = null;
+    Sprite playSprite = null;
 
 
-
-    private Button volumeButton = null;
+    private Button button = null;
     private bool volumeOn = true;
+    private bool isPaused = false;
 
-    // Start is called before the first frame update
     void Start()
     {
-        volumeButton = GetComponent<Button>();
+        button = GetComponent<Button>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -27,10 +29,18 @@ public class MenuButton : MonoBehaviour
 
     public void VolumeButtonClicked()
     {
-        volumeButton.image.sprite = volumeOn ? volumeOffSprite : volumeOnSprite;
+        if(button != null)
+		{
+            if(volumeOnSprite == null)
+			{
+                volumeOnSprite = Resources.Load<Sprite>("Sprites/UI/VolumeOnSprite");
+                volumeOffSprite = Resources.Load<Sprite>("Sprites/UI/VolumeOffSprite");
+			}
+            button.image.sprite = volumeOn ? volumeOffSprite : volumeOnSprite;
+            volumeOn = !volumeOn;
+		}
 
-        volumeOn = !volumeOn;
-
+        AudioListener.volume = volumeOn ? 1.0f : 0.0f;
     }
 
     public void LeaderBoardButtonClicked()
@@ -40,18 +50,54 @@ public class MenuButton : MonoBehaviour
 
     public void EasyButtonClicked()
     {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        SceneManager.LoadScene("GameScene");
 
     }
 
     public void MediumButtonClicked()
     {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        SceneManager.LoadScene("GameScene");
 
     }
 
     public void HardButtonClicked()
     {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        SceneManager.LoadScene("GameScene");
 
     }
 
+    public void QuitGameClicked()
+	{
+        //switch back to main menu
+        Screen.orientation = ScreenOrientation.Portrait;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void PauseButtonClicked()
+	{
+        if (button != null)
+        {
+            if (playSprite == null)
+            {
+                playSprite = Resources.Load<Sprite>("Sprites/UI/PlaySprite");
+                pauseSprite = Resources.Load<Sprite>("Sprites/UI/PauseSprite");
+            }
+            button.image.sprite = isPaused ? playSprite : pauseSprite;
+            isPaused = !isPaused;
+        }
+    }
+
+    public void InstructionButtonClicked()
+	{
+        Instantiate(Resources.Load("Prefabs/InstructionsPanel"));
+    }
+
+    public void CloseInstructionButton()
+	{
+        Destroy(GetComponentInParent<Image>().gameObject);
+	}
 
 }
