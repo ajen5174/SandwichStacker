dce306f618d5a07a547ff2fa16f1e3a4d5d60562
Andrew Jensen <AJensen@student.neumont.edu>
2020-12-09 19:38:24 -0700

    Implement Leader Board Scene

--- Assets/Scripts/IngredientSpawner.cs
+++ Assets/Scripts/IngredientSpawner.cs
@@ -7,15 +7,21 @@ public class IngredientSpawner : MonoBehaviour
     [SerializeField] float spawnSpeed = 2f;
     float spawnTimer = 0f;
 
+    public bool isPaused = false;
+
     private void Update()
     {
-        spawnTimer += Time.deltaTime;
-
-        if (spawnTimer >= spawnSpeed)
+        if(!isPaused)
         {
-            spawnTimer = 0f;
-            SpawnIngredient();
+            spawnTimer += Time.deltaTime;
+
+            if (spawnTimer >= spawnSpeed)
+            {
+                spawnTimer = 0f;
+                SpawnIngredient();
+            }
         }
+        
     }
 
     private void SpawnIngredient()
