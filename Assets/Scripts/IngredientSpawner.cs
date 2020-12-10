8549555094d47fd3835450e10cfe50521e26544f
GaigeKinsey <gaigekinsey@yahoo.com>
2020-12-09 19:21:41 -0700

    Gets the one of the needed ingredients 75% of the time

--- Assets/Scripts/IngredientSpawner.cs
+++ Assets/Scripts/IngredientSpawner.cs
@@ -5,6 +5,7 @@ public class IngredientSpawner : MonoBehaviour
 {
     [SerializeField] public Ingredient[] ingredients;
     [SerializeField] float spawnSpeed = 2f;
+    [SerializeField] SandwichCreator sandwichCreator = null;
     float spawnTimer = 0f;
     float gravityScale = 0.5f;
 
@@ -39,7 +40,17 @@ public class IngredientSpawner : MonoBehaviour
 
     private void SpawnIngredient()
     {
-        Ingredient ingredient = ingredients[UnityEngine.Random.Range(0, ingredients.Length)];
+        Ingredient ingredient = null;
+        // Get the needed ingredients 75% of the time
+        if (UnityEngine.Random.Range(0, 100) > 75)
+        {
+            int index = (int) sandwichCreator.ingredientGoal[UnityEngine.Random.Range(0, sandwichCreator.ingredientGoal.Count)];
+            ingredient = ingredients[index];
+        }
+        else
+        {
+            ingredient = ingredients[UnityEngine.Random.Range(0, ingredients.Length)];
+        }
 
         Ingredient spawned = Instantiate(ingredient, new Vector3(UnityEngine.Random.Range(-5f, 5f), 6), Quaternion.identity);
         Rigidbody2D rb = spawned.GetComponent<Rigidbody2D>();
